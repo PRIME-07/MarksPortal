ebd6e8b3e3d7fae86505fbca5aa1e7598eb1df52
Amritesh Hiras <Amritesh3R@gmail.com>
2025-01-24 12:20:10 +0530

    Updated logic for validating Email

--- src/utils/validateEmail.js
+++ src/utils/validateEmail.js
@@ -1,5 +1,5 @@
 const validateEmail = (email) => {
-    const emailRegex = /^[^\s@_0-9]+_202[5678]@woxsen.edu.in/;
+    const emailRegex = /^[^\s@_0-9]+@woxsen.edu.in/;
     return emailRegex.test(email);
 };
 
